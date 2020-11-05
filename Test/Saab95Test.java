d8659eecf68d5820304ab84b3c48f2b83c4290b6
erra-ch <>
2020-11-05 13:46:27 +0100

    Setup

---
+++ Test/Saab95Test.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+public class Saab95Test {
+
+
+    /** Description */
+ /*   @Test
+    public void testName(){
+        yeeet;
+
+        assertTrue(aaa == iii);
+    }
+
+  */
+
+
+
+
+
+
+
+}
---
+++ Test/Volvo240Test.java
@@ -0,0 +1,14 @@
+public class Volvo240Test {
+
+
+
+
+
+
+
+
+
+
+
+
+}
